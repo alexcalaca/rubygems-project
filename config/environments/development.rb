fb6ee9163408bbdc8f7c729de93e8edbd12b8946
Ubuntu <ubuntu@ip-172-31-9-236.eu-central-1.compute.internal>
2020-08-11 15:39:32 +0000

    Commit the project

---
+++ config/environments/development.rb
@@ -0,0 +1,50 @@
+Rails.application.configure do
+  config.hosts << "622130778a5743d99bb74132e4841163.vfs.cloud9.eu-central-1.amazonaws.com"
+  config.cache_classes = false
+
+  config.eager_load = false
+
+  config.consider_all_requests_local = true
+
+  if Rails.root.join('tmp', 'caching-dev.txt').exist?
+    config.action_controller.perform_caching = true
+    config.action_controller.enable_fragment_cache_logging = true
+
+    config.cache_store = :memory_store
+    config.public_file_server.headers = {
+      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+    }
+  else
+    config.action_controller.perform_caching = false
+
+    config.cache_store = :null_store
+  end
+
+  config.active_storage.service = :local
+  
+  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.perform_caching = false
+
+  config.active_support.deprecation = :log
+
+  config.active_record.migration_error = :page_load
+
+  # Highlight code that triggered database queries in logs.
+  config.active_record.verbose_query_logs = true
+
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.debug = true
+
+  # Suppress logger output for asset requests.
+  config.assets.quiet = true
+
+  # Raises error for missing translations.
+  # config.action_view.raise_on_missing_translations = true
+
+  # Use an evented file watcher to asynchronously detect changes in source code,
+  # routes, locales, etc. This feature depends on the listen gem.
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+end
